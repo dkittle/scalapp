3c72417d29d1dba6990872ac35c9b9a1b9697e01
dkittle <don@kittle.ca>
2016-08-31 18:08:45 -0400

    clean up Employee case class and object

--- app/models/Employee.scala
+++ app/models/Employee.scala
@@ -5,7 +5,7 @@ import java.time.LocalDateTime
 import utils.JsonUtils
 
 case class Employee(
-                     id: Option[Long],
+                     id: Long,
                      name: String,
                      address: String,
                      dateOfBirth: LocalDateTime,
@@ -17,6 +17,5 @@ case class Employee(
 object Employee extends JsonUtils {
 
   implicit def createEmployee(json: String): Employee = fromJson[Employee](json)
-//  implicit def writeEmployee(employee: Employee): String = toJson(employee)
 
 }
